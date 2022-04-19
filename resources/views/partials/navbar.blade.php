b9188874b81dd5c795d99c1f564db5d21bec1ec7
SaputraAfryan <afryan54@gmail.com>
2022-04-19 23:34:40 +0700

    Update Navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -4,16 +4,6 @@
         <img src="logoABP.png" alt="logo.png" width="125" height="95" class="d-inline-block align-text-middle">
         Easycation
       </a>
-      <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown button
-        </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </div>
       <div class="d-flex flex-row-reverse">
         <div class="p-2">
           <a href="/signup" class="btn btn-secondary" role="button">Sign Up</a>
